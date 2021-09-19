6478e0ba421d6eb0de3c1b455685b92e65b4beaf
Subhajit Mondal <40147428+direct2subhajit@users.noreply.github.com>
2021-09-19 23:52:05 +0530

    Add files via upload

---
+++ Screen_Recorder_UsingCV/screen_recorder.py
@@ -0,0 +1,20 @@
+
+#Screen recorder#
+
+import cv2
+import numpy as np
+import pyautogui
+SCREEN_SIZE = (1920, 1080)
+fourcc= cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+while True:
+    img = pyautogui.screenshot()
+    frame= np. array(img)
+    frame= cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    out.write(frame)
+    #if the user clicks w, it exits
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+cv2.destroyAllWindows()
+out.release()
